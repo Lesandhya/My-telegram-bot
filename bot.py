dcb941afa3efaffce88e8a47347106ad93ee99f6
Lesandhya <142742298+Lesandhya@users.noreply.github.com>
2025-07-01 15:07:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # /start command function
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🙏 ସ୍ବାଗତ! ମୁଁ ତୁମର ସାଧାରଣ Telegram Bot।")
+    keyboard = [
+        [InlineKeyboardButton("📢 Join Our Channel", url="https://t.me/+2CwhuwoujTNiNGU0")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    await update.message.reply_text(
+        "🙏 ଧନ୍ୟବାଦ! ଆପଣ ଆମ Telegram Bot ରେ ସ୍ୱାଗତ।",
+        reply_markup=reply_markup
+    )
 
 # Bot setup
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token("8005999110:AAHz5aDnOpMzcmSdYUilymCDIq0J95I71Tc").build()
 
     application.add_handler(CommandHandler("start", start))
-
     application.run_polling()
